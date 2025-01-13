7d2ddacb4ebd8fe469c628837e377c2021b46dc6
Šandera Filip <L20220082@prumyslovkajihlava.cz>
2025-01-13 11:16:38 +0100

    first react

---
+++ ReactTest/src/component/CountApp.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './CountApp.css'
+
+function CountApp() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default CountApp
